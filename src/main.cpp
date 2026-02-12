070a59403eebd39d6df8481eb5dae90e5bb41d1a
onjamm <azmionjang@gmail.com>
2026-02-12 19:51:48 +0000

    got the background from the slidesj

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,41 @@
 
 #include "base_conv.h"
 
-int main() {
+int main()
+{
     bn::core::init();
 
+    int bin_num = 0b1010010;
+    int mask = 0b10101101;
+
+    // int num = 86;
+
+    // int anded = bin_num & mask;
+    // int ored = bin_num | mask;
+
+    // BN_LOG(to_string_base(ored, 2));
+    // BN_LOG(to_string_base(anded, 2));
+
+    // BN_LOG(bin_num);
+    // BN_LOG(to_string_base(num, 2));
+
+    // Like the sout in java
     BN_LOG("Hey!");
 
-    bn::backdrop::set_color(bn::color());
+    int color = 0; // 0b0'00000'00000'11111;
+    int blue = 6 << 10;
+    int green = 15 << 5;
+    int red = 20;
 
-    while(true) {
+    color = blue | green | red;
+    bn::backdrop::set_color(bn::color(color));
+
+    BN_LOG(to_string_base(blue, 2));
+    BN_LOG(to_string_base(green, 2));
+    BN_LOG(to_string_base(red, 2));
+
+    while (true)
+    {
         bn::core::update();
     }
 }
